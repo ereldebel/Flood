90a94d09fd68ac8bc338d425a429be96bb902738
ereldebel <erel.debel@mail.huji.ac.il>
2022-03-19 23:18:01 +0200

    Added water rise when collided with. the gun can now point downward, there is currently a problem with the limits.

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -1,18 +1,41 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Water : MonoBehaviour
 {
-	// [SerializeField] private float waterRiseTime = 5;
-	// [SerializeField] private float waterRiseSpeed = 5;
+	[SerializeField] private float waterRiseTime = 5;
+	[SerializeField] private float waterRiseSpeed = 5;
+
+	private float _currentRiseFinishTime;
+	private Transform _transform;
+
+	private void Awake()
+	{
+		_transform = transform;
+		waterRiseSpeed /= Time.fixedDeltaTime;
+	}
+
 	private void OnCollisionEnter(Collision collision)
 	{
 		collision.gameObject.SetActive(false);
-		// StartCoroutine(RaiseWater());
+		if (Time.time >= _currentRiseFinishTime)
+		{
+			_currentRiseFinishTime = Time.time + waterRiseTime;
+			StartCoroutine(RaiseWater());
+			return;
+		}
+		_currentRiseFinishTime += waterRiseTime;
 	}
 
-	// private IEnumerator RaiseWater()
-	// {
-	// 	
-	// 	yield return new WaitForUpdate();
-	// }
+	private IEnumerator RaiseWater()
+	{
+		while (Time.time < _currentRiseFinishTime)
+		{
+			var pos = transform.position;
+			pos.y += waterRiseSpeed;
+			_transform.position = pos;
+			yield return new WaitForFixedUpdate();
+		}
+	}
 }
