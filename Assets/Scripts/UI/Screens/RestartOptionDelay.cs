32175c21ccd478894b82505cdf07dd58821fbd6a
ereldebel <erel.debel@mail.huji.ac.il>
2022-04-26 18:45:13 +0300

    updated game to wait a second before allowing replay

---
+++ Assets/Scripts/UI/Screens/RestartOptionDelay.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace UI.Screens
+{
+	public class RestartOptionDelay : MonoBehaviour
+	{
+		private StartOnClick _startOnClick;
+		private TextMeshProUGUI _replayText;
+		[SerializeField] private float delayTime = 1;
+
+		private void Awake()
+		{
+			_startOnClick = GetComponent<StartOnClick>();
+			_replayText = GetComponent<TextMeshProUGUI>();
+		}
+
+		private void Update()
+		{
+			if (Time.time < delayTime) return;
+			_startOnClick.enabled = true;
+			_replayText.enabled = true;
+			Destroy(this);
+		}
+	}
+}
